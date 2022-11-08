24ca08f8e6b1e5398ae96f5768ef36318f9a8a25
Rick Arko <rarko@shipbob.com>
2022-11-08 05:03:41 +0000

    add baseline

--- src/baseline.py
+++ src/baseline.py
@@ -1,4 +1,7 @@
+import time
+
 import pandas as pd
+from loguru import logger
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import GridSearchCV
 
@@ -12,22 +15,36 @@ gb_params = {
 gb_class = GradientBoostingClassifier()
 gb_grid = GridSearchCV(gb_class, gb_params, cv = 5, n_jobs=-1)
 
+
 if __name__ == '__main__':
+    START = time.time()
     LABEL = "target"
-    X_train = pd.read_parquet("data/X_train.snap.parquet").head(1000)
-    X_test = pd.read_parquet("data/X_test.snap.parquet").head(1000)
-    y_train = pd.read_parquet("data/y_train.snap.parquet")[LABEL].head(1000)
-    y_test = pd.read_parquet("data/y_test.snap.parquet")[LABEL].head(1000)
+    import os
+    os.makedirs("Logs", exist_ok=True)
+    logger.add(f"Logs/baseline-processor.log", 
+               format="{time:YYYY-MM-DD HH:mm:ss} LogLevel {level} {message}", level="INFO")
+    logger.info(f"Begin processing data for baseline model")
+
+    X_train = pd.read_parquet("data/X_train.snap.parquet")
+    X_test = pd.read_parquet("data/X_test.snap.parquet")
+    y_train = pd.read_parquet("data/y_train.snap.parquet")[LABEL]
+    y_test = pd.read_parquet("data/y_test.snap.parquet")[LABEL]
     submission = pd.read_csv('data/sample_submission.csv')
+    
+    logger.info(f"Read X_train: {X_train.shape} and X_test: {X_test.shape}")
 
     gb_grid.fit(X_train, y_train)
-    gb_grid.best_estimator_
-    gb_opt = GradientBoostingClassifier(criterion='friedman_mse', init=None,
-                                        learning_rate=0.1, loss='deviance', max_depth=3,
-                                        max_features=None, max_leaf_nodes=None, min_impurity_split=None,
+    logger.info(f"Best Estimator: {gb_grid.best_estimator_}")
+
+    gb_opt = GradientBoostingClassifier(criterion='friedman_mse',
+                                        init=None,
+                                        learning_rate=0.1, loss='log_loss', max_depth=3,
+                                        max_features=None, max_leaf_nodes=None,
+                                        # min_impurity_split=None,
                                         min_samples_leaf=1, min_samples_split=2,
                                         min_weight_fraction_leaf=0.0, n_estimators=100,
-                                        presort='auto', random_state=None, subsample=1.0, verbose=0,
+                                        # presort='auto',
+                                        random_state=None, subsample=1.0, verbose=0,
                                         warm_start=False)
         
     gb_opt.fit(X_train, y_train)
@@ -37,5 +54,5 @@ if __name__ == '__main__':
 
     gb_out['target'] = 1-gb_out['target']
     gb_out.to_csv('data/gb_predictions1.csv', index=False, float_format='%.4f')
-
+    logger.info(f"Finished Baseline model in {time.time() - START:,.1} seconds.")
     
