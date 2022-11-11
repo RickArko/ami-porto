e3ec5ce5cc9810d6539bd172aa6e596c4a00d6dc
Rick Arko <rarko@shipbob.com>
2022-11-11 03:07:29 +0000

    add lightgbm modelling notebook

--- src/score.py
+++ src/score.py
@@ -18,31 +18,39 @@ DIR = Path("data")
 FNAME_BASE = DIR.joinpath("baseline_predictions_cv.csv")
 # FNAME_BASE = DIR.joinpath("data/baseline_predictions.csv")
 FNAME_MODEL = DIR.joinpath("prediction.snap.parquet")
+FNAME_MODEL = DIR.joinpath("cv_scores.snap.parquet")
 
 dfbase = pd.read_csv(FNAME_BASE).rename(
     columns={"pred": "pred_baseline", "pred_label": "label_baseline"}
 )
 
+THRESH = .03
 dfpred = pd.read_parquet(FNAME_MODEL)
-dfbase = dfbase.merge(dfpred[["id"] + [PRED_MODEL, LABEL_MODEL]], on="id", how="inner")
+if "target" in dfpred.columns and PRED_MODEL not in dfpred.columns:
+    dfpred = dfpred.rename(columns={"target": PRED_MODEL})
+if LABEL_MODEL not in dfpred.columns:
+    dfpred[LABEL_MODEL] = np.where(dfpred[PRED_MODEL] >= THRESH, 1, 0)
+    
+dfscore = dfbase.merge(dfpred[["id"] + [PRED_MODEL, LABEL_MODEL]], on="id", how="inner")
+
 
 if __name__ == "__main__":
 
     os.makedirs(PLOT_DIR, exist_ok=True)
 
-    dfbase["pred_naive"] = 0
+    dfscore["pred_naive"] = 0
 
-    acc_naive = accuracy_score(dfbase[LABEL], dfbase["pred_naive"]) * 100
-    acc_baseline = accuracy_score(dfbase[LABEL], dfbase[LABEL_BASELINE]) * 100
-    acc_model = accuracy_score(dfbase[LABEL], dfbase[LABEL_MODEL]) * 100
+    acc_naive = accuracy_score(dfscore[LABEL], dfscore["pred_naive"]) * 100
+    acc_baseline = accuracy_score(dfscore[LABEL], dfscore[LABEL_BASELINE]) * 100
+    acc_model = accuracy_score(dfscore[LABEL], dfscore[LABEL_MODEL]) * 100
 
-    rep_naive = classification_report(dfbase[LABEL], dfbase["pred_naive"])
-    rep_baseline = classification_report(dfbase[LABEL], dfbase[LABEL_BASELINE])
-    rep_model = classification_report(dfbase[LABEL], dfbase[LABEL_MODEL])
+    rep_naive = classification_report(dfscore[LABEL], dfscore["pred_naive"])
+    rep_baseline = classification_report(dfscore[LABEL], dfscore[LABEL_BASELINE])
+    rep_model = classification_report(dfscore[LABEL], dfscore[LABEL_MODEL])
 
-    roc_naive = roc_auc_score(dfbase[LABEL], dfbase["pred_naive"])
-    roc_base = roc_auc_score(dfbase[LABEL], dfbase[LABEL_BASELINE])
-    roc_model = roc_auc_score(dfbase[LABEL], dfbase[LABEL_MODEL])
+    roc_naive = roc_auc_score(dfscore[LABEL], dfscore["pred_naive"])
+    roc_base = roc_auc_score(dfscore[LABEL], dfscore[LABEL_BASELINE])
+    roc_model = roc_auc_score(dfscore[LABEL], dfscore[LABEL_MODEL])
 
     print(f"""Accuracy (Naive): {acc_naive:.3f}%""")
     print(f"""Accuracy (Baseline): {acc_baseline:.3f}%""")
@@ -57,9 +65,9 @@ if __name__ == "__main__":
     print(f"""Classification Report (Model): {rep_model}""")
 
     # Generate ROC Curve Comparison
-    fpr_naive, tpr_naive, _ = roc_curve(dfbase[LABEL], dfbase["pred_naive"])
-    fpr_baseline, tpr_baseline, _ = roc_curve(dfbase[LABEL], dfbase[PRED_BASELINE])
-    fpr, tpr, _ = roc_curve(dfbase[LABEL], dfbase[PRED_MODEL])
+    fpr_naive, tpr_naive, _ = roc_curve(dfscore[LABEL], dfscore["pred_naive"])
+    fpr_baseline, tpr_baseline, _ = roc_curve(dfscore[LABEL], dfscore[PRED_BASELINE])
+    fpr, tpr, _ = roc_curve(dfscore[LABEL], dfscore[PRED_MODEL])
 
     plt.plot(fpr_naive, tpr_naive, label=f"Naive {roc_naive:.3f}", alpha=0.7, color="black", linestyle="--")
     plt.plot(fpr_baseline, tpr_baseline, label=f"Baseline {roc_base:.3f}", color="Blue", alpha=0.7)
